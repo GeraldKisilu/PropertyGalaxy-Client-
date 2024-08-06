4dcee1285ce970e0a5434b08918844d5f6783e5f
kamusarihanshi <ahmedizaka9@gmail.com>
2024-08-06 12:31:43 +0300

    agentmessages component

--- src/AgentProperties.jsx
+++ src/AgentProperties.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
 
 function AgentProperty({ property}) {
