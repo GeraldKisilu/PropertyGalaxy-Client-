085ed911eebf6863a8b3a77844d333625b09a274
amandasuzzanne <amandasuzanne12@gmail.com>
2024-08-15 09:40:10 +0300

    Refactor code

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -18,6 +18,7 @@ const HomePage = () => {
     const [suggestions, setSuggestions] = useState([]);
     const [boostedProperties, setBoostedProperties] = useState([]);
     const [notification, setNotification] = useState(null); // State for notification
+    const [showContactCard, setShowContactCard] = useState(false);
 
     useEffect(() => {
         fetch('http://127.0.0.1:5050/property/list')
@@ -51,6 +52,10 @@ const HomePage = () => {
         setLocation(event.target.value);
     };
 
+    const toggleContactCard = () => {
+        setShowContactCard(!showContactCard);
+    };
+
     const handleBoostSuccess = (city, price, image) => {
         setNotification({ city, price, image }); // Set notification with city, price, and image
     };
@@ -70,67 +75,23 @@ const HomePage = () => {
                     onClose={handleCloseNotification}
                 />
             )}
-            <header className="header">
-                <div className="overlay"></div>
-                <div className="header-top">
-                    <div className="container">
-                        <div className="wrapper">
-                            {/* <ul className="header-top-social-list">
-                                <li><a href="#" className="header-top-social-link"><ion-icon name="logo-facebook"></ion-icon></a></li>
-                                <li><a href="#" className="header-top-social-link"><ion-icon name="logo-twitter"></ion-icon></a></li>
-                                <li><a href="#" className="header-top-social-link"><ion-icon name="logo-instagram"></ion-icon></a></li>
-                                <li><a href="#" className="header-top-social-link"><ion-icon name="logo-pinterest"></ion-icon></a></li>
-                            </ul> */}
-                        </div>
-                    </div>
-                </div>
-            </header>
 
             <header className="navbar">
-                <div className="navbar-content">
-                    <div className="navbar-brand">Property Galaxy</div>
-                    {/* <ul className="navbar-social-list">
-                        <li><a href="#" className="navbar-social-link"><ion-icon name="logo-facebook"></ion-icon></a></li>
-                        <li><a href="#" className="navbar-social-link"><ion-icon name="logo-twitter"></ion-icon></a></li>
-                        <li><a href="#" className="navbar-social-link"><ion-icon name="logo-instagram"></ion-icon></a></li>
-                        <li><a href="#" className="navbar-social-link"><ion-icon name="logo-pinterest"></ion-icon></a></li>
-                        </ul> */}
+                <div>
+                    <div className="app-name">Property Galaxy</div>
                 </div>
-
-
+                
                 <nav className="navbar-links">
                     <Link to="/">Home</Link>
                     <Link to="/properties">Properties</Link>
                     <Link to="/reviews">Reviews</Link>
-
-                    <Link to="/apply-agents">Do you wanna be an agent?</Link>
-
                     <Link to="/favourites-page">❤️ Favorites</Link>
-
-
+                    <Link to="/apply-agents">Become an agent?</Link>
+                    <Link to="/profile">Profile </Link>
                 </nav>
             </header>
-            <Link to="/profile">
-                <button className="header-top-btn">Profile</button>
-            </Link>
 
             <main className="body-content">
-                {/* Boosted Properties Section at the top */}
-                <section className="boosted-properties">
-                    <h2>Boosted Properties</h2>
-                    <div className="boosted-properties-grid">
-                        {boostedProperties.map(property => (
-                            <div className="boosted-property-item" key={property.id}>
-                                <Link to={`/property/${property.id}`} className="favorites-link">
-                                    {property.address}, {property.city} - ${property.price}
-                                </Link>
-
-
-
-                            </div>
-                        ))}
-                    </div>
-                </section>
 
                 <div className="search-bar">
                     <input
@@ -148,30 +109,53 @@ const HomePage = () => {
                     <button onClick={handleSearch}>Search</button>
                 </div>
 
+                {/* Boosted Properties Section at the top */}
+                <section className="boosted-properties">
+                    <h2>Boosted Properties</h2>
+                    <div className="boosted-properties-grid">
+                        {boostedProperties.map(property => (
+                            <div className="boosted-property-item" key={property.id}>
+                                <Link to={`/property/${property.id}`} className="favorites-link">
+                                    {property.address}, {property.city} - ${property.price}
+                                </Link>
+                            </div>
+                        ))}
+                    </div>
+                </section>
+
                 <div className="hero-featured-property">
                     <section className="hero" id="home">
-                        <div className="container">
                             <div className="hero-content">
                                 <h2 className="hero-title">Find Your Dream House By Us</h2>
                                 <p className="hero-text">
                                     Find your dream house with ease at Property Galaxy, where diverse listings and expert guidance meet your real estate needs. Explore, discover, and make informed decisions with us today!
                                 </p>
-                                <button className="contact-agent-button">📞 Contact Agent</button>
+                                <button className="contact-agent-button" onClick={toggleContactCard}>📞 Contact Agent</button>
+                                    {showContactCard && (
+                                        <div className="contact-card">
+                                            <a href="mailto:info@propertygalaxy.com" className="contact-email">
+                                                <ion-icon name="mail-outline"></ion-icon>
+                                                <span>info@propertygalaxy.com</span>
+                                            </a>
+                                            <p>📞 +254 718 338217</p>
+                                        </div>
+                                    )}                            
                             </div>
-                        </div>
                     </section>
 
                     <div className="featured-property">
                         <div className="property-details">
-                            <h2>Featured Property</h2>
                             <img src={image1} alt="Property Galaxy" className="featured-property-image" />
-                            <p>Price: $500,000</p>
-                            <p>Description: This is a beautiful house located in a serene environment. It features modern amenities and spacious rooms.</p>
+                            <div className="property-gallery">
+                                {/* Additional images can be added here */}
+                            </div>
                         </div>
                     </div>
                 </div>
 
-                <Link to="/properties" className="view-all-button">View All Properties</Link>
+                <div className='view-btn-container'>
+                    <Link to="/properties" className="view-all-button">View All Properties</Link>
+                </div>
 
                 <section className="about" id="about">
                     <div className="container">
@@ -180,7 +164,7 @@ const HomePage = () => {
                             <img src={interior} alt="House interior" className="abs-img" />
                         </figure>
                         <div className="about-content">
-                            <p className="section-subtitle">About Us</p>
+                            <p className="title">About Us</p>
                             <h2 className="h2 section-title">The Leading Real Estate Rental Marketplace.</h2>
                             <p className="about-text">
                                 Over 30,000 people work for us in more than 70 countries all over the world. This breadth of global coverage, combined with specialist services.
@@ -214,16 +198,14 @@ const HomePage = () => {
                             <p className="callout">
                                 "The only place that can make you comfortable enough to stay by yourself, with your companion and your whole extended family."
                             </p>
-                            <a href="#service" className="btn">Our Services</a>
                         </div>
                     </div>
                 </section>
 
                 {/* Service Section */}
                 <section className="service" id="service">
-                    <div className="container">
-                        <p className="section-subtitle">Our Services</p>
-                        <h2 className="h2 section-title">Our Main Focus</h2>
+                        <p className="service-title">Our Services</p>
+                        <h2 className="service-subtitle">Our Main Focus</h2>
                         <div className="service-list">
                             <div className="service-card">
                                 <div className="card-icon">
@@ -235,7 +217,7 @@ const HomePage = () => {
                                 <p className="card-text">
                                     We offer professional consulting services to help you make informed decisions about your property investments.
                                 </p>
-                                <Link to="/properties" className="view-all-button">Read More</Link>
+                                <Link to="/properties" className="service-button">Read More</Link>
 
                             </div>
                             <div className="service-card">
@@ -245,7 +227,7 @@ const HomePage = () => {
                                 <h3 className="card-title"><a href="#">Sell Property </a></h3>
 
                                 <p className="card-text">Our property management services ensure that your real estate assets are well-maintained and profitable.</p>
-                                <Link to="/agents" className="view-all-button">Read More</Link>
+                                <Link to="/agents" className="service-button">Read More</Link>
 
                             </div>
                             <div className="service-card">
@@ -254,17 +236,16 @@ const HomePage = () => {
                                 </div>
                                 <h3 className="card-title"><a href="#">Rent Property</a></h3>
                                 <p className="card-text">We provide in-depth market analysis to help you understand the real estate trends and make the right investment choices.</p>
-                                <Link to="/properties" className="view-all-button">Read More</Link>
+                                <Link to="/properties" className="service-button">Read More</Link>
                             </div>
                         </div>
-                    </div>
                 </section>
 
             </main>
 
             {/* Footer Section */}
             <footer className="footer">
-                <ul className="header-top-list">
+                {/* <ul className="header-top-list">
                     <li>
                         <a href="mailto:info@propertygalaxy.com" className="header-top-link">
                             <ion-icon name="mail-outline"></ion-icon>
@@ -277,9 +258,9 @@ const HomePage = () => {
                             <address>15/A, Nairobi, Kenya</address>
                         </a>
                     </li>
-                </ul>
+                </ul> */}
                 <div className="footer-links">
-                    <span>Download our app:</span>
+                    <span style={{backgroundColor: '#f5f5f5'}}>Download our app:</span>
                     <span className="footer-icon">📱 Google Play</span>
                     <span className="footer-icon">🍏 App Store</span>
                     <span className="footer-icon">💳 Visa</span>
